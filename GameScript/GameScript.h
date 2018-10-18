c46e0c762b874c04d9e72156989974387563df02
chenchuang28 <874938129@qq.com>
2018-10-18 21:19:21 +0800

    Initial Commit

---
+++ GameScript/GameScript.h
@@ -0,0 +1,19 @@
+//
+//  GameScript.h
+//  GameScript
+//
+//  Created by 陈创 on 2018/10/18.
+//  Copyright © 2018年 陈创. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GameScript.
+FOUNDATION_EXPORT double GameScriptVersionNumber;
+
+//! Project version string for GameScript.
+FOUNDATION_EXPORT const unsigned char GameScriptVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GameScript/PublicHeader.h>
+
+
