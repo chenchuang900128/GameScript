3958c8cf1ac457e2d2da9fadfa23ed1d0818a937
chenchuang28 <874938129@qq.com>
2018-10-23 19:09:05 +0800

    增加注释

--- CustomProgress.h
+++ CustomProgress.h
@@ -9,15 +9,21 @@
 #import <UIKit/UIKit.h>
 
 @interface CustomProgress : UIView
-
+// 背景图片
 @property(nonatomic, strong)UIImageView *bgimg;
+// 左边进度条图片
 @property(nonatomic, strong)UIImageView *leftimg;
+// 进度条上要展示的文字
 @property(nonatomic, strong)UILabel *presentlab;
 
+// 进度条最大值
 @property(nonatomic,assign)float maxValue;
 
+//设置进度条值
 -(void)setProgress:(NSUInteger)progress;
 
+// 显示
 - (void)show;
+// 隐藏
 - (void)hide;
 @end
